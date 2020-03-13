4799466933d464b55d99be4507b3edb7cd45de57
rosettaplus007 <kowndiboyalakuntla@gmail.com>
2020-03-13 23:04:17 +0530

    thirs commit

--- news.java
+++ news.java
@@ -1,9 +1,3 @@
-<p><div style="display: flex;"><div style="flex: 1;"><time datetime="2014-09-20" class="icon">
-            <em>Saturday</em>
-            <strong>September  <br>2020</strong>
-            <span>20</span>
-          </time></div>
-<div style="flex: 8;color: black;font-size: 1.5rem;vertical-align: middle;padding-top: 2.5rem;">Flash News! Flash News!</div>
-</div>
-
-</p>
+<p><div style="display: flex;"><div style="flex: 1;"><time datetime="2014-09-20" class="icon"><em>Saturday</em><strong>September  <br>2020</strong><span>20</span></time></div><div style="flex: 8;color: black;font-size: 1.5rem;vertical-align: middle;padding-top: 2.5rem;">Flash News! Flash News!</div></div></p>
+<p><div style="display: flex;"><div style="flex: 1;"><time datetime="2014-09-20" class="icon"><em>Saturday</em><strong>September  <br>2020</strong><span>20</span></time></div><div style="flex: 8;color: black;font-size: 1.5rem;vertical-align: middle;padding-top: 2.5rem;">Flash News! Flash News!</div></div></p>
+<p><div style="display: flex;"><div style="flex: 1;"><time datetime="2014-09-20" class="icon"><em>Saturday</em><strong>September  <br>2020</strong><span>20</span></time></div><div style="flex: 8;color: black;font-size: 1.5rem;vertical-align: middle;padding-top: 2.5rem;">Flash News! Flash News!</div></div></p>
